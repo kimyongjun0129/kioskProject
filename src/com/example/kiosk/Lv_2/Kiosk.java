7a50e2bbc0136f58244a599b0dcf7c79f7d5c8a5
kimyongjun0129 <kimyongjun0129@naver.com>
2025-04-24 15:59:29 +0900

    Lv_2 키오스크

--- src/com/example/kiosk/Lv_2/Kiosk.java
+++ src/com/example/kiosk/Lv_2/Kiosk.java
@@ -1,10 +1,32 @@
 package com.example.kiosk.Lv_2;
 
-public class Kiosk {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Kiosk {
+    // 속성
+    List<MenuItem> foodList = new ArrayList<>();
+
+    // 생성자
+
+    // 기능
     // 메뉴 추가하는 기능
-    private void addMenu() {
+    public void addMenu(String foodName, double price, String ingredient) {
+        foodList.add(new MenuItem(foodName, price, ingredient));
     }
 
     // 메뉴 보여주는 기능
+    public void showMenu() {
+        int num = 1;
+
+        System.out.println(" [ SHAKESHACK MENU ]");
+        System.out.println("0. 종료\t\t\t\t | 종료");
+        for (MenuItem foodInfo : foodList) {
+            System.out.println(num++ + ". " + foodInfo.getFood_name() + "\t\t | w " + foodInfo.getPrice() + "\t " + foodInfo.getIngredients());
+        }
+    }
+
+    public double totalFoodCount() {
+        return (long) foodList.size();
+    }
 }
--- src/com/example/kiosk/Lv_2/Main.java
+++ src/com/example/kiosk/Lv_2/Main.java
@@ -1,7 +1,32 @@
 package com.example.kiosk.Lv_2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("[ SHAKESHACK MENU ]");
+        Kiosk kiosk = new Kiosk();
+
+        kiosk.addMenu("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        kiosk.addMenu("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+        kiosk.addMenu("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+        kiosk.addMenu("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거");
+
+        Scanner scanner = new Scanner(System.in);
+        int input = 1;
+
+        do {
+            try {
+                kiosk.showMenu();
+                System.out.print("메뉴를 선택하세요 : ");
+                input = scanner.nextInt();
+
+                if(input > kiosk.totalFoodCount()) {
+                    System.out.println("음식이 없습니다.");
+                }
+            } catch (RuntimeException e) {
+                System.out.println("올바른 값을 입력하세요.");
+                scanner.nextLine();
+            }
+        } while(input != 0);
     }
 }
