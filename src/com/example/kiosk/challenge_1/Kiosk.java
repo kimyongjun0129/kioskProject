462a77f9638e69333c0d2b52606e5200617e0eaa
kimyongjun0129 <kimyongjun0129@naver.com>
2025-04-27 10:56:10 +0900

    도전 Lv_1 Kiosk 클래스 수정

--- src/com/example/kiosk/challenge_1/Kiosk.java
+++ src/com/example/kiosk/challenge_1/Kiosk.java
@@ -40,21 +40,23 @@ public class Kiosk {
                 }
 
                 // 장바구니 메뉴가 보이지 않을 때, 전체 메뉴 갯수 보다 큰 수를 입력 시 예외 생성
-                if (shoppingBascket.getList().isEmpty() && mainMenuInput > menu.getTotalMenuCount()) throw new InputMismatchException();
+                if (shoppingBascket.getList().isEmpty() && mainMenuInput > menu.getTotalMenuCount()) throw new IndexOutOfBoundsException();
                 // 장바구니가 보일 때, 전체 메뉴 갯수 + 장바구니 메뉴 갯수(2) 보다 큰 수를 입력 시 예외 생성
-                if (mainMenuInput > menu.getTotalMenuCount()+2) throw new InputMismatchException();
+                if (mainMenuInput > menu.getTotalMenuCount() + 2) throw new IndexOutOfBoundsException();
 
                 // 장바구니 확인
-                if (mainMenuInput == menu.getTotalMenuCount()+1) {
+                if (mainMenuInput == menu.getTotalMenuCount() + 1) {
                     shoppingBascket.printTotalMenu();
 
-                    System.out.println("1. 주문\t\t2.메뉴판");
+                    System.out.println("1. 주문\t\t2. 메뉴판");
                     System.out.print("주문하시겠습니까? ");
                     int input = userInput();
-                    if(!(input > 0 && input < 3)) throw new InputMismatchException();
+                    if (!(input > 0 && input < 3)) throw new IndexOutOfBoundsException();
 
                     // 주문 완료
                     if (input == 1) {
+                        int num = 1;
+
                         System.out.println("주문이 완료되었습니다. 금액은 w " + shoppingBascket.getSum() + " 입니다.\n");
                         shoppingBascket.resetSum();
                         shoppingBascket.resetshoppingBascket();
@@ -63,57 +65,44 @@ public class Kiosk {
                 }
 
                 // 주문 취소
-                if (mainMenuInput == menu.getTotalMenuCount()+2) {
+                if (mainMenuInput == menu.getTotalMenuCount() + 2) {
                     System.out.println("주문이 취소되었습니다.\n");
                     shoppingBascket.resetshoppingBascket();
                     continue;
                 }
-            } catch (InputMismatchException e) {
-                System.out.println("없는 메뉴입니다.\n");
-                continue;
-            }
 
-            // 메뉴를 보여준다.
-            System.out.println();
-            menu.showMenu(mainMenuInput);
+                // 메뉴를 보여준다.
+                System.out.println();
+                menu.showMenu(mainMenuInput);
 
-            // 메뉴에 대한 사용자의 입력을 받는다.
-            System.out.print("메뉴를 선택하세요 : ");
-            try {
+                // 메뉴에 대한 사용자의 입력을 받는다.
+                System.out.print("메뉴를 선택하세요 : ");
                 menuInput = userInput();
                 if (menuInput == 0) {
                     System.out.println();
                     continue;
                 }
-            } catch (InputMismatchException e) {
-                System.out.println("잘못 입력했습니다.");
-                continue;
-            }
 
-            // 선택된 메뉴 보여주기
-            try {
+                // 선택된 메뉴 보여주기
                 menu.showSelectedFood(mainMenuInput, menuInput);
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("음식이 존재하지 않습니다.\n");
-                continue;
-            }
 
-            // 장바구니에 추가할건지에 대한 사용자의 입력받기
-            System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
-            System.out.println("1. 확인\t\t2. 취소");
-            try {
+                // 장바구니에 추가할건지에 대한 사용자의 입력받기
+                System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+                System.out.println("1. 확인\t\t2. 취소");
                 System.out.print("추가하시겠습니까? ");
                 shoppingBascketInput = userInput();
-                if(!(shoppingBascketInput > 0 && shoppingBascketInput < 3)) throw new InputMismatchException();
+                if (!(shoppingBascketInput > 0 && shoppingBascketInput < 3)) throw new IndexOutOfBoundsException();
 
                 // 장바구니에 추가하기
-                if(shoppingBascketInput == 1) {
+                if (shoppingBascketInput == 1) {
                     shoppingBascket.addMenu(menu.getMenuItems(mainMenuInput, menuInput));
                 } else {
                     System.out.println("취소되었습니다.\n");
                 }
-            } catch (InputMismatchException e) {
+            } catch (IndexOutOfBoundsException e) {
                 System.out.println("없는 메뉴입니다.\n");
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력해주세요.\n");
             }
         } while(true);
     }
