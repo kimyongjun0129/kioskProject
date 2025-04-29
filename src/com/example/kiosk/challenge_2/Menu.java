75eb8ba2c5d7a85026b88a9fa16af9f803cf5959
kimyongjun0129 <kimyongjun0129@naver.com>
2025-04-29 17:31:37 +0900

    menu 삭제

--- src/com/example/kiosk/challenge_2/Menu.java
+++
@@ -1,8 +0,0 @@
-package com.example.kiosk.challenge_2;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Menu {
-    private final List<List<MenuItem>> menuItems = new ArrayList<>();
-}
